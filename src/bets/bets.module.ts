8217d68cd93e598dbc4b1393b33fd35480c44455
motazsamoud <motaz.sammoud@esprit.tn>
2025-10-14 11:05:44 +0100

    feat: finishhhhhhhh333333333333

--- src/bets/bets.module.ts
+++ src/bets/bets.module.ts
@@ -5,16 +5,13 @@ import { BetsService } from './bets.service';
 import { BetsController } from './bets.controller';
 import { WalletModule } from '../wallet/wallet.module';
 import {JwtModule} from "@nestjs/jwt";
+import {UserModule} from "src/user/user.module";
 
 @Module({
     imports: [
         WalletModule,
         MongooseModule.forFeature([{ name: Bet.name, schema: BetSchema }]),
-        JwtModule.register({
-            secret: process.env.JWT_SECRET ||  'your-secret-key',  // Use a more secure secret in production
-            signOptions: { expiresIn: '1h' },
-            // Set token expiry (e.g., 1 hour)
-        }),
+        UserModule,
     ],
     providers: [BetsService],
     controllers: [BetsController],
